018d9fbbc3042b72b91ca4a7472d67598ba9ca53
rurutheGeek <126381911+rurutheGeek@users.noreply.github.com>
2023-08-15 10:54:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
@@ -6,8 +5,10 @@ import discord
 from discord import app_commands
 from discord.ext import tasks
 
+activity = discord.Activity(name='キノコのほうし', type=discord.ActivityType.playing) #アクティビティの設定
 client = discord.Client(intents=discord.Intents.all(),activity=activity)
+tree = app_commands.CommandTree(client)
 
-//ここにコードを書く
+#ここにコードを書く
 
 client.run(os.environ.get('DISCORD_TOKEN'))
