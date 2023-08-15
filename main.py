84ff223b909ce43607c16288574f1c8e8749d675
rurutheGeek <126381911+rurutheGeek@users.noreply.github.com>
2023-08-15 10:47:42 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import os
+import discord
+from discord import app_commands
+from discord.ext import tasks
+
+client = discord.Client(intents=discord.Intents.all(),activity=activity)
+
+// ‚±‚±‚ÉƒR[ƒh‚ğ‘‚­
+
+client.run(os.environ.get('DISCORD_TOKEN'))
