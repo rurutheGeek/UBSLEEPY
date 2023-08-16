1d89c6bd0c196017a0e64670863f17ff3af1f45b
rurutheGeek <126381911+rurutheGeek@users.noreply.github.com>
2023-08-17 01:48:38 +0900

    Update booting.sh

--- booting.sh
+++ booting.sh
@@ -3,9 +3,6 @@
 #start_app.serviceで実行されるスクリプトファイル
 #sudo systemctl enable start_app.serviceで有効化
 
-echo ...Bot booting...
-cd /home/ruru/UBSLEEPY
 git --git-dir=.git pull origin main
 nohup python3 /home/ruru/UBSLEEPY/main.py &
 nohup /home/ruru/actions-runner/run.sh &
-exit 0
