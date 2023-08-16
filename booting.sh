94c746c6342f80907edac25b0813a0e3b413b2de
rurutheGeek <126381911+rurutheGeek@users.noreply.github.com>
2023-08-17 01:54:06 +0900

    Delete booting.sh

--- booting.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#start_app.serviceで実行されるスクリプトファイル
-#sudo systemctl enable start_app.serviceで有効化
-
-git --git-dir=.git pull origin main
-nohup python3 /home/ruru/UBSLEEPY/main.py &
-nohup /home/ruru/actions-runner/run.sh &
